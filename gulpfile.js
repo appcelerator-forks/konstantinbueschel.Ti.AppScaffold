20a5a28ddee055610086194ae29e39abb3c662c1
Konstantin Büschel <konstantin@die-interaktiven.de>
2014-09-16 16:28:17 +0200

    Erweiterung der Gulp Tasks, sodass nun die Bower Dateien in die korrekten Verzeichnisse des App Systems kopiert werden.

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,34 @@
+
 'use strict';
 
-
-var gulp =          require('gulp'),
-    $ =             require('gulp-load-plugins')(),
-    browserify =    require('browserify'),
-    browserSync =   require('browser-sync'),
-    del =           require('del'),
-    Pageres =       require('pageres'),
-    wiredep =       require('wiredep').stream,
-    args =          require('yargs').argv,
-    runSequence =   require('run-sequence');
+var gulp =              require('gulp'),
+    $ =                 require('gulp-load-plugins')(),
+    del =               require('del'),
+    args =              require('yargs').argv,
+    preen =             require('preen'),
+    runSequence =       require('run-sequence'),
+    bowerFiles =        require('main-bower-files');
 
 
 /**********************************************************************************
  * CONFIGURATION
  * ********************************************************************************/
 var CONFIG = {
+
     DIRECTORIES: {
-        "DEPLOYMENT": "dist",
-        "DOCUMENTATION": "doc",
-        "SOURCE": "src",
-        "TEMP": ".tmp",
-        "SCREENSHOTS": "screenshots",
-        "BOWER": "src/bower_components"
+
+        BOWER: './bower_components',
+        BOWER_DESTINATION_MAP: {
+
+        }
     },
-    BROWSERS: ["last 2 version"],
-    URL: {
-        DEVELOPMENT: 'http://www.google.de',
-        PRODUCTION: 'http://www.google.de'
-    },
+
     ENVIRONMENT: args.env || args.environment || 'development'
 };
 
 
 var isDevelopmentEnv = CONFIG.ENVIRONMENT === 'development',
     isProductionEnv = CONFIG.ENVIRONMENT === 'production';
-
-
-/**********************************************************************************
- * SERVE
- * ********************************************************************************/
-gulp.task('serve', function () {
-
-    browserSync({
-        server: {
-            baseDir: [CONFIG.DIRECTORIES.SOURCE , CONFIG.DIRECTORIES.TEMP]
-        },
-        notify: false
-    });
-
-    gulp.watch([CONFIG.DIRECTORIES.SOURCE + '/**/*.html'], browserSync.reload);
-    gulp.watch([CONFIG.DIRECTORIES.SOURCE + '/scss/**/*.scss'], ['scss:dev']);
-    gulp.watch([CONFIG.DIRECTORIES.TEMP + '/css/**/*.css'], browserSync.reload);
-    gulp.watch([CONFIG.DIRECTORIES.SOURCE + '/js/**/*.js'], ['jshint:dev']);
-    gulp.watch([CONFIG.DIRECTORIES.SOURCE + '/img/**/*'], ['imagemin']);
-});
 
 
 /**********************************************************************************
@@ -79,98 +53,70 @@ gulp.task('scss', function () {
 
 });
 
-/**********************************************************************************
- * BROWSERIFY
- *********************************************************************************/
-gulp.task('browserify', function(){
-    browerify(CONFIG.DIRECTORIES.SOURCE + '/js/main.js')
-        .bundle();
-});
-/**********************************************************************************
- * JSHINT:DEVELOPMENT
- *********************************************************************************/
-gulp.task('jshint:dev', function () {
-    gulp.src(CONFIG.DIRECTORIES.SOURCE + '/js/**/*.js')
-        .pipe($.jshint('.jshintrc'))
-        .pipe($.jshint.reporter('jshint-stylish'));
-});
-
-
-/**********************************************************************************
- * WIREDEP
- * ********************************************************************************/
-gulp.task('wiredep', function () {
-    gulp.src(CONFIG.DIRECTORIES.SOURCE + '/index.html')
-        .pipe(wiredep({}))
-        .pipe(gulp.dest(CONFIG.DIRECTORIES.SOURCE));
-});
-
-/**********************************************************************************
- * IMAGHEMIN
- * ********************************************************************************/
-gulp.task('imagemin', function () {
-    gulp.src(CONFIG.DIRECTORIES.SOURCE + '/img/**')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [
-                {removeViewBox: false}
-            ]
-        }))
-        .pipe(gulp.dest(CONFIG.DIRECTORIES.DEPLOYMENT + '/img'));
-});
-
-gulp.task('pageres', function () {
-
-
-    var pageres = new Pageres({delay: 2})
-        .src(CONFIG.URL.PRODUCTION)
-        .dest(CONFIG.DIRECTORIES.SCREENSHOTS);
-
-    pageres.run(function (err) {
-        if (err) {
-            throw err;
-        }
-    });
-
-});
-
-
-gulp.task('uncss', function () {
-    /*
-     gulp.src('bootstrap.css')
-     .pipe($.uncssTask({
-     html: ['index.html', 'contact.html', 'about.html']
-     }))
-     .pipe(gulp.dest('dest'));
-     */
-});
-
-gulp.task('kss', function () {
-    gulp.src(CONFIG.DIRECTORIES.SOURCE + '/scss/**/*.scss')
-        .pipe($.kss())
-        .pipe(gulp.dest(CONFIG.DIRECTORIES.DOCUMENTATION + '/styleguide/'));
-});
-
 
 /**********************************************************************************
  * CLEAN
  * ********************************************************************************/
-gulp.task('clean', del.bind(null, [CONFIG.DIRECTORIES.TEMP, CONFIG.DIRECTORIES.DEPLOYMENT]));
+gulp.task('clean', function(callback) {
+
+    // delete Bower dir
+    del([CONFIG.DIRECTORIES.BOWER + '*'], callback);
+});
+
+gulp.task('preen', ['bower'], function(callback) {
+
+    // return preen.preen({}, callback);
+    preen.preen({}, callback);
+});
+
+
+/**********************************************************************************
+ * INIT
+ * ********************************************************************************/
+gulp.task('bower', function() {
+
+    return $.bower();
+});
+
+gulp.task('init', ['bower', 'preen'], function() {
+
+    var momentFilter =  $.filter('moment-with-locales.js'),
+        lodashFilter =  $.filter('q.js'),
+        queueFilter =   $.filter('lodash.min.js'),
+        asyncFilter =   $.filter('async.js');
+
+
+    return gulp.src(bowerFiles({
+                env: CONFIG.ENVIRONMENT
+            }))
+
+            .pipe(momentFilter)
+            .pipe($.rename('moment.js'))
+            .pipe(gulp.dest('Resources/helpers/date'))
+            .pipe(momentFilter.restore())
+
+            .pipe(lodashFilter)
+            .pipe($.rename('lodash.js'))
+            .pipe(gulp.dest('Resources/helpers/common'))
+            .pipe(lodashFilter.restore())
+
+            .pipe(queueFilter)
+            .pipe($.rename('q.js'))
+            .pipe(gulp.dest('Resources/helpers/xhr'))
+            .pipe(queueFilter.restore())
+
+            .pipe(asyncFilter)
+            .pipe(gulp.dest('Resources/helpers/xhr'))
+            .pipe(asyncFilter.restore());
+});
 
 
 /**********************************************************************************
  * DEFAULT
  * ********************************************************************************/
-gulp.task('default', ['clean'], function () {
-    runSequence('scss:dev');
-});
+gulp.task('default', function(callback) {
 
-
-/**********************************************************************************
- * DEPLOY
- * ********************************************************************************/
-gulp.task('deploy', function () {
-    runSequence('scss:deploy');
+    runSequence('init', 'clean', callback);
 });
 
 
